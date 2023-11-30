842bcfbdb902d1ade6c4bb2e92439aa0f3ed9ae5
surbhi-kuhar <surbhikuhar07@gmail.com>
2023-11-30 17:57:38 +0530

    Functionality of booking one appointment per patient per date

--- src/HomePage.java
+++ src/HomePage.java
@@ -8,9 +8,7 @@ import java.util.Scanner;
 public class HomePage {
     private static final String url ="jdbc:mysql://localhost:3306/bookyourappointment";
     private static final String username = "root";
-
     private static final String password = "Supergirl7)";
-
     public static void main(String args[]){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -210,68 +208,65 @@ public class HomePage {
 
     private static synchronized void bookAppointment(Connection connection, Scanner sc, SignUp patient, Doctor doctor, String department, int pid) {
 
-        int patient_id = 0;
-        boolean validPatientId = false;
+            int patient_id = 0;
+            boolean validPatientId = false;
 
-        // Loop until a valid patient id is entered
-        while (!validPatientId) {
-            System.out.println("\nEnter patient id");
-            patient_id = sc.nextInt();
+            // Loop until a valid patient id is entered
+            while (!validPatientId) {
+                System.out.println("\nEnter patient id");
+                patient_id = sc.nextInt();
 
-            if (patient_id != pid) {
-                System.out.println("Enter your own patient id");
-            } else {
-                validPatientId = true;
-            }
-        }
-
-            sc.nextLine();
-
-            System.out.println("\nEnter doctor name");
-            String doctor_name = sc.nextLine();
-
-        int doctor_id=0;
-        boolean validDoctorId = false;
-
-        // Loop until a valid doctor id is entered
-        while (!validDoctorId) {
-            System.out.println("\nEnter doctor id");
-            doctor_id = sc.nextInt();
-
-            if (!doctor.getDoctorByName(doctor_name, department, doctor_id)) {
-                System.out.println("You entered wrong doctor id.");
-            } else {
-                validDoctorId = true;
-            }
-        }
-
-            if(!doctor.getDoctorByName(doctor_name,department,doctor_id)){
-                System.out.println("You entered wrong doctor id.");
-
-            }
-
-        LocalDate appointment_date = null;
-        boolean validAppointmentDate = false;
-        String appointment_date_str = null;
-
-        // Loop for a valid appointment date
-        while (!validAppointmentDate) {
-            System.out.println("\nEnter appointment date (YYYY-MM-DD):");
-            appointment_date_str = sc.next();
-
-            try {
-                appointment_date = LocalDate.parse(appointment_date_str);
-                LocalDate now = LocalDate.now();
-
-                if (appointment_date.isAfter(now)) {
-                    validAppointmentDate = true;
+                if (patient_id != pid) {
+                    System.out.println("Enter your own patient id");
                 } else {
-                    System.out.println("Enter a date that is after today's date.");
+                    validPatientId = true;
                 }
-            } catch (DateTimeParseException e) {
-                System.out.println("Invalid date format. Please enter the date in the format (YYYY-MM-DD).");
             }
-        }
+
+
+            boolean validDoctorInfo = false;
+
+            String doctor_name = null;
+            int doctor_id=0;
+
+            // Loop until a valid doctor id is entered
+            while (!validDoctorInfo) {
+                sc.nextLine(); // Consume the newline character before reading the doctor name
+                System.out.println("\nEnter name of doctor (note: enter the same name as shown)");
+                doctor_name = sc.nextLine();
+
+                System.out.println("\nEnter doctor id");
+                doctor_id = sc.nextInt();
+
+                if (!doctor.getDoctorByName(doctor_name, department, doctor_id)) {
+                    System.out.println("You entered wrong doctor id or doctor name");
+                } else {
+                    validDoctorInfo = true;
+                }
+            }
+
+            LocalDate appointment_date = null;
+            boolean validAppointmentDate = false;
+            String appointment_date_str = null;
+
+            // Loop for a valid appointment date
+            while (!validAppointmentDate) {
+                System.out.println("\nEnter appointment date (YYYY-MM-DD):");
+                appointment_date_str = sc.next();
+
+                try {
+                    appointment_date = LocalDate.parse(appointment_date_str);
+                    LocalDate now = LocalDate.now();
+
+                    if (appointment_date.isAfter(now)) {
+                        validAppointmentDate = true;
+                    } else {
+                        System.out.println("Enter a date that is after today's date.");
+                    }
+                } catch (DateTimeParseException e) {
+                    System.out.println("Invalid date format. Please enter the date in the format (YYYY-MM-DD).");
+                }
+            }
 
 
                 // check if patient and doctor exist
@@ -279,30 +274,37 @@ public class HomePage {
                     // check if doctor is available or not
                     if (doctor.getDoctorByName(doctor_name, department, doctor_id)) {
                         if (checkDoctorAvailability(doctor_name, appointment_date_str, connection)) {
-                            String appointment_query = "INSERT INTO appointments(patient_id,doctor_id,appointment_date, department,doctor_name) VALUES (?,?,?,?,?)";
-                            try {
-                                PreparedStatement preparedStatement = connection.prepareStatement(appointment_query);
-                                preparedStatement.setInt(1, patient_id);
-                                preparedStatement.setInt(2, doctor_id);
-                                preparedStatement.setString(3, appointment_date_str);
-                                preparedStatement.setString(4, department);
-                                preparedStatement.setString(5, doctor_name);
-                                int rowsAffected = preparedStatement.executeUpdate();
 
-                                if (rowsAffected > 0) {
-                                    System.out.println("\n\n------------------------------Appointment Booked-----------------------------");
-                                    System.out.println("View previous appointments? (Yes/No)");
+                            if(!checkAppointmentOnTheGivenDate(patient_id,appointment_date_str,department,connection)) {
 
-                                    String input = sc.next();
-                                    if (input.equalsIgnoreCase("Yes")) {
-                                        previousAppointments(patient_id, connection, sc,patient,doctor);
+                                String appointment_query = "INSERT INTO appointments(patient_id,doctor_id,appointment_date, department,doctor_name) VALUES (?,?,?,?,?)";
+                                try {
+                                    PreparedStatement preparedStatement = connection.prepareStatement(appointment_query);
+                                    preparedStatement.setInt(1, patient_id);
+                                    preparedStatement.setInt(2, doctor_id);
+                                    preparedStatement.setString(3, appointment_date_str);
+                                    preparedStatement.setString(4, department);
+                                    preparedStatement.setString(5, doctor_name);
+                                    int rowsAffected = preparedStatement.executeUpdate();
+
+                                    if (rowsAffected > 0) {
+                                        System.out.println("\n\n------------------------------Appointment Booked-----------------------------");
+                                        System.out.println("View previous appointments? (Yes/No)");
+
+                                        String input = sc.next();
+                                        if (input.equalsIgnoreCase("Yes")) {
+                                            previousAppointments(patient_id, connection, sc, patient, doctor);
+                                        }
+
+                                    } else {
+                                        System.out.println("Failed to book appointment");
                                     }
-
-                                } else {
-                                    System.out.println("Failed to book appointment");
+                                } catch (SQLException e) {
+                                    e.printStackTrace();
                                 }
-                            } catch (SQLException e) {
-                                e.printStackTrace();
+                            }
+                            else{
+                                System.out.println("You already have an appointment booked on this date in this department. Try booking on some other date.");
                             }
                         } else {
                             System.out.println("\nOops!! Doctor is not available on this date. Try on some other date.");
@@ -315,7 +317,25 @@ public class HomePage {
                 }
             }
 
+            private static boolean checkAppointmentOnTheGivenDate(int patient_id, String appointment_date, String department, Connection connection){
+                String query = "select * from appointments where patient_id=? and appointment_date=? and department=?";
+               try{
+                   PreparedStatement preparedStatement = connection.prepareStatement(query);
+                   preparedStatement.setInt(1,patient_id);
+                   preparedStatement.setString(2, appointment_date);
+                   preparedStatement.setString(3,department);
+                   ResultSet resultSet = preparedStatement.executeQuery();
+                   if(resultSet.next()){
+                       return true;
+                   }
+                   return false;
+               }
+               catch(SQLException e){
+                   e.printStackTrace();
+               }
 
+               return false;
+            }
 
     private static boolean checkDoctorAvailability(String doctor_name, String appointment_date,Connection connection){
         String query = "select COUNT(*) from appointments where doctor_name=? AND appointment_date=?";
@@ -377,5 +397,6 @@ public class HomePage {
         }
     }
 
+    // one user should be able to make only one appointment per day
 
 }
